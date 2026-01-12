a93e7e53fb7becff6d3e452abef61c63a05dd90c
Melvin Stepan <fdai7783@informatik.hs-fulda.de>
2026-01-13 00:29:14 +0100

    rollback to clean state 9dc873a

--- README.md
+++ README.md
@@ -18,6 +18,3 @@ sudo chmod 644 /etc/rancher/rke2/rke2.yaml
 
 kubectl get all -n immich //für immich
 kubectl get all -n projectcontour //für ingress-controller (envoy)
-
-#Pipeline
-Die Pipeline in GitLab ist so konfiguriert, dass sie bei jedem Push auf den main-Branch ausgeführt wird.
--- main.tf
+++ main.tf
@@ -1,45 +1,31 @@
-############################
-# Variables (GitHub Secrets)
-############################
-variable "os_project" {
-  type = string
-
-}
-
-variable "os_username" {
-  type = string
-}
-
-variable "os_password" {
-  type = string
-
-}
-
-# Inhalte aus GitHub Secrets (keine Dateipfade!)
-variable "ssh_public_key" {
-  type = string
-}
-
+###########################################################
+# 
+#
+###########################################################
 
 locals {
-  insecure         = true
-  auth_url         = "https://private-cloud.informatik.hs-fulda.de:5000"
-  object_store_url = "https://10.32.4.32:443"
-  region           = "RegionOne"
-  cacert_file      = "./os-trusted-cas"
+    # Konfiguration
+    insecure         = true
+    auth_url         = "https://private-cloud.informatik.hs-fulda.de:5000"
+    object_store_url = "https://10.32.4.32:443"
+    region           = "RegionOne"
+    cacert_file      = "./os-trusted-cas"
 
-  cluster_name     = lower("${var.os_project}-k8s")
-  image_name       = "ubuntu-22.04-jammy-server-cloud-image-amd64"
-  flavor_name      = "m1.medium"
-  system_user      = "ubuntu"
-  floating_ip_pool = "ext_net"
+    cluster_name     = lower("${var.project}-k8s")
+    image_name       = "ubuntu-22.04-jammy-server-cloud-image-amd64"
+    flavor_name      = "m1.medium"
+    system_user      = "ubuntu"
+    floating_ip_pool = "ext_net"
+    
+    # SSH Keys
+    ssh_pubkey_file  = "~/.ssh/id_ed25519.pub"
+    # für den Upload der Dateien
+    ssh_private_key  = "~/.ssh/id_ed25519" 
 
-  dns_server   = "10.33.16.100"
-  rke2_version = "v1.30.3+rke2r1"
-
-  kubeconfig_path = "${path.module}/${lower(var.os_project)}-k8s.rke2.yaml"
-
-  ssh_private_key  = "~/.ssh/id_ed25519" 
+    dns_server       = "10.33.16.100"
+    rke2_version     = "v1.30.3+rke2r1"
+    
+    kubeconfig_path = "${path.module}/${lower(var.project)}-k8s.rke2.yaml"
 }
 
 module "rke2" {
@@ -48,39 +34,40 @@ module "rke2" {
   insecure            = local.insecure
   bootstrap           = true
   name                = local.cluster_name
-  ssh_authorized_keys = [var.ssh_public_key]
+  ssh_authorized_keys = [file(local.ssh_pubkey_file)]
   floating_pool       = local.floating_ip_pool
-
-  rules_ssh_cidr = ["0.0.0.0/0"]
-  rules_k8s_cidr = ["0.0.0.0/0"]
+  rules_ssh_cidr      = [ "0.0.0.0/0" ]
+  rules_k8s_cidr      = [ "0.0.0.0/0" ]
 
   servers = [{
-    name               = "controller"
-    flavor_name        = local.flavor_name
-    image_name         = local.image_name
-    system_user        = local.system_user
-    boot_volume_size   = 6
-    rke2_version       = local.rke2_version
-    rke2_volume_size   = 10
+    name = "controller"
+    flavor_name = local.flavor_name
+    image_name  = local.image_name
+    system_user = local.system_user
+    boot_volume_size = 6
+    rke2_version     = local.rke2_version
+    rke2_volume_size = 10
     rke2_volume_device = "/dev/vdb"
-    rke2_config        = <<EOF
+    rke2_config = <<EOF
 write-kubeconfig-mode: "0644"
 EOF
   }]
 
-  agents = [{
-    name               = "worker"
-    nodes_count        = 1
-    flavor_name        = local.flavor_name
-    image_name         = local.image_name
-    system_user        = local.system_user
-    boot_volume_size   = 10
-    rke2_version       = local.rke2_version
-    rke2_volume_size   = 90
-    rke2_volume_device = "/dev/vdb"
-  }]
+  agents = [
+    {
+      name        = "worker"
+      nodes_count = 1
+      flavor_name = local.flavor_name
+      image_name  = local.image_name
+      system_user = local.system_user
+      boot_volume_size = 10
+      rke2_version     = local.rke2_version
+      rke2_volume_size = 100
+      rke2_volume_device = "/dev/vdb"
+    }
+  ]
 
-  backup_schedule  = "0 6 1 * *"
+  backup_schedule  = "0 6 1 * *" 
   backup_retention = 20
 
   kube_apiserver_resources = {
@@ -95,7 +82,7 @@ EOF
   etcd_resources = {
     requests = { cpu = "75m", memory = "128M" }
   }
-
+  
   dns_nameservers4    = [local.dns_server]
   ff_autoremove_agent = "30s"
   ff_write_kubeconfig = true
@@ -107,10 +94,14 @@ EOF
 
   registries = {
     mirrors = {
-      "*" = { endpoint = ["https://harbor.cs.hs-fulda.de"] }
+      "*": { endpoint = ["https://harbor.cs.hs-fulda.de"] }
     }
   }
 }
+
+variable "project" { type = string }
+variable "username" { type = string }
+variable "password" { type = string }
 
 output "floating_ip" {
   value = module.rke2.external_ip
@@ -118,9 +109,9 @@ output "floating_ip" {
 
 provider "openstack" {
   insecure    = local.insecure
-  tenant_name = var.os_project
-  user_name   = var.os_username
-  password    = var.os_password
+  tenant_name = var.project
+  user_name   = var.username
+  password    = var.password
   auth_url    = local.auth_url
   region      = local.region
   cacert_file = local.cacert_file
